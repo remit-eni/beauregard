8397f0a8bec492d43ca6d7b7dbb379af1f6408cb
Rémi Treussart <remi.treussart2020@campus-eni.fr>
2021-05-20 12:06:36 +0200

    Premier commit

---
+++ src/main/java/fr/acme/beauregard_project/BeauregardProjectApplication.java
@@ -0,0 +1,13 @@
+package fr.acme.beauregard_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeauregardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeauregardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/acme/beauregard_project/BeauregardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.acme.beauregard_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeauregardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
